8a6532b5408008cc338dac648962b29115ac20c9
stephecloutier <stephecloutier@gmail.com>
2017-02-16 11:43:28 +0100

    Add value attribute to the form

--- views/_form.php
+++ views/_form.php
@@ -1,10 +1,24 @@
 <form action="index.php" method="post">
     <label for="nbRow">Quels chiffres voulez-vous&nbsp? (Rangés/Colonnes)</label>
 
-    <input type="text" name="nbRow" id="nbRow">
+    <input
+        type="text"
+        name="nbRow"
+        id="nbRow"
+        <?php if($nbRow !==0): ?>
+            value="<?= $nbRow; ?>"
+        <?php endif; ?>
+    >
     <p><?= $errors['nbRow'] ?></p>
 
-    <input type="text" name="nbCol" id="nbCol">
+    <input
+        type="text"
+        name="nbCol"
+        id="nbCol"
+        <?php if($nbCol !==0): ?>
+            value="<?= $nbCol; ?>"
+        <?php endif; ?>
+    >
     <p><?= $errors['nbCol'] ?></p>
 
     <input type="submit" value="Calculer">
